098a4fa044c0d4c68f36d217a8d0fe940bf6d0de
Dominik Tobschall <dominik@fruux.com>
2014-08-17 21:51:13 +0200

    fixed example. thx @evert

--- example/server.php
+++ example/server.php
@@ -32,12 +32,12 @@ $server->on('connect', function($socket) use ($server) {
     echo 'Connected: ' . $socket->getName() . PHP_EOL;
     echo 'We have ' . count($server->getClients()) . ' connected client(s).' . PHP_EOL;
 
-    $socket->on('disconnect', function($socket) use ($server, $socket) {
+    $socket->on('disconnect', function($socket) use ($server) {
         echo 'Disconnected: ' . $socket->getName() . PHP_EOL;
         echo 'We have ' . count($server->getClients()) . ' client(s) left.' . PHP_EOL;
     });
 
-    $socket->on('data', function($socket, $data) use ($server, $socket) {
+    $socket->on('data', function($socket, $data) use ($server) {
         if(count($server->getClients()) > 1) {
             $socket->send('Thank you, ' . $socket->getName() . '. Message received and broadcasted.' . PHP_EOL);
             $server->broadcast($socket->getName() . ' sent: ' . $data . PHP_EOL);
